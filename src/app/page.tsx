032365a600cc4749fd2a52fb59bb031ecb5d3265
Merus23 <mateus.satira@gmail.com>
2024-09-20 11:19:28 -0300

    feat: finish the get all products logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,73 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+import { Product } from "./Interfaces/IProducts";
+
 export default function Home() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleGetAllProducts = async () => {
+    try {
+      setIsLoading(true);
+      const response = await fetch(`http://127.0.0.1:8000/api/produtos`, {
+        method: "GET",
+      });
+
+      if (response.ok) {
+        const responseData = await response.json();
+        setProducts(responseData);
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    handleGetAllProducts();
+  }, []);
+
   return (
     <main className="flex flex-col gap-8 md:w-11/12  mx-auto pt-4">
       <h1 className="text-3xl">Lista de produtos</h1>
       <div className="flex">
-        <div className="flex-1 b">
+        <div className="flex-1 flex flex-col gap-10">
           <h2 className="text-2xl ">Produtos:</h2>
-          <ol></ol>
+          {isLoading ? (
+            <div
+              className="inline-block h-48 w-48 animate-spin rounded-full border-4 border-solid border-black border-e-transparent align-[-0.125em] text-black dark:text-white"
+              role="status"
+            >
+              <span className="sr-only">Loading...</span>
+            </div>
+          ) : (
+            <ol>
+              {products.map((product) => (
+                <li key={product.cod_produto}>
+                  <article>
+                    <details>
+                      <summary className="text-lg font-bold">
+                        Nome: {product.nome_produto}
+                      </summary>
+
+                      <div className="pl-4 flex flex-col">
+                        <ol>
+                          <li>Valor: {product.valor_produto}</li>
+                          <li>Estoque: {product.estoque}</li>
+                          <li>Cidade: {product.cidade.nome_cidade}</li>
+                          <li>Marca: {product.marca.nome_marca}</li>
+                        </ol>
+                      </div>
+                    </details>
+                  </article>
+                </li>
+              ))}
+            </ol>
+          )}
         </div>
 
-        <details className="text-black flex-1" name="">
+        <details className="text-black w-1/5" name="">
           <summary className="text-2xl">Filtros</summary>
 
           <div className="flex flex-col w-1/2 pl-4 gap-1">
